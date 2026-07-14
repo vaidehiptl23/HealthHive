73be4f620510a0b27e6a3314b1534317acc8643e
Vaidehi Patel <vaidehit04@gmail.com>
2026-07-14 23:37:29 +0530

    Redirect Android build directory to C:/healthhive_build to resolve Windows path space errors

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,11 +5,11 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+val newBuildDir = layout.projectDirectory.dir("C:/healthhive_build")
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
